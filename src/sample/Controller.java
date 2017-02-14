e5fd9c442228452b1074cc10a3c1e6ab4ec2e395
Rob Culclasure <robculclasure@Robs-MacBook-Pro.local>
2017-02-14 15:56:57 -0600

    Added writeJson method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,12 +10,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
+import jodd.json.JsonSerializer;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
-
     @FXML
     ListView list;
     @FXML
@@ -30,19 +33,26 @@ public class Controller implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         list.setItems(contacts);
-
     }
 
-    public void addContact(){
+    public void addContact() throws IOException {
         Contact contact = new Contact(name.getText(), phone.getText(), email.getText());
         contacts.add(contact);
-    }
-    public void removeContact(){
-        Contact contact = (Contact) list.getSelectionModel().getSelectedItem();
-        contacts.remove(contact);
+        writeJson();
     }
 
-//    public void writeJson(){
-//
-//    }
+    public void removeContact() throws IOException {
+        Contact contact = (Contact) list.getSelectionModel().getSelectedItem();
+        contacts.remove(contact);
+        writeJson();
+    }
+
+    public void writeJson() throws IOException {
+        File f = new File("people.json");
+        JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.serialize(contacts);
+        FileWriter fw = new FileWriter(f);
+        fw.write(json);
+        fw.close();
+    }
 }
